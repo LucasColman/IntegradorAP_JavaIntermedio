dd5ab59b8c8c9e22cda23c65490659a49ed931bc
LucasColman <lukasscolman@gmail.com>
2023-11-17 16:38:46 -0300

    Carga inicial

---
+++ src/main/java/com/mycompany/reportedeincidentes/modelo/Cliente.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.reportedeincidentes.modelo;
+
+import java.util.List;
+
+/**
+ *
+ * @author Lucas
+ */
+public class Cliente {
+    private String idCliente;
+    private String nombre;
+    private String apellido;
+    private String razonSocialString;
+    private String cuil;
+
+    
+    
+  
+}
